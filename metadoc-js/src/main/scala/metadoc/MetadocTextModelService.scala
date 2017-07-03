ebd632c13a6b5d89bad6815c53871edf019610eb
Jonas Fonseca <jonas.fonseca@gmail.com>
2017-07-03 09:32:48 -0400

    Group model reference requests by file
    Removes the need for the custom model cache and reduces the number of
    requests considerably.
    
    References #39

--- metadoc-js/src/main/scala/metadoc/MetadocTextModelService.scala
+++ metadoc-js/src/main/scala/metadoc/MetadocTextModelService.scala
@@ -16,19 +16,6 @@ import monaco.services.ITextModelResolverService
 
 @ScalaJSDefined
 object MetadocTextModelService extends ITextModelResolverService {
-  // NOTE: we have a private cache here to avoid "duplicate model" errors.
-  // It's possible to hit those errors for example in the reference provider, where
-  // we call `Future.sequence(Seq.map(modelReference))`. Since js is single threaded,
-  // we are safe from race conditions.
-  private val cache = mutable.Map.empty[String, IModel]
-  def createModel(value: String, filename: String): IModel =
-    createModel(value, createUri(filename))
-  def createModel(value: String, uri: Uri): IModel =
-    cache.getOrElseUpdate(
-      uri.path,
-      monaco.editor.Editor.createModel(value, "scala", uri)
-    )
-
   def modelReference(
       filename: String
   ): Future[IReference[ITextEditorModel]] =
@@ -44,7 +31,7 @@ object MetadocTextModelService extends ITextModelResolverService {
       for {
         attrs <- MetadocAttributeService.fetchProtoAttributes(resource.path)
       } yield {
-        val model = createModel(attrs.contents, resource)
+        val model = Editor.createModel(attrs.contents, "scala", resource)
         new ImmortalReference(ITextEditorModel(model))
       }
     }
