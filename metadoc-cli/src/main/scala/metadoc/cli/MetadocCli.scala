56880474623169df1387c330ffb8624f8cdb97ee
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-09-13 21:45:28 +0200

    Fix #52, refactor CLI.
    - much faster performance by handling one .semanticdb at a time instead
      of loading everything at once
    - operate on proto messages instead of fancy langmeta data structures
    - process .semanticdb files in parallel
    - progress bar to know something is happening.
    
    Building the site for the community build (~1M LOC):
    ```
    31.75s user 12.83s system 136% cpu 32.565 total
    ```
    
    This commit changes the scheme of metadoc.index to only include
    filenames, not Index.symbols. This breaks the web UI, which needs
    to be fixed in a separate commit.

--- metadoc-cli/src/main/scala/metadoc/cli/MetadocCli.scala
+++ metadoc-cli/src/main/scala/metadoc/cli/MetadocCli.scala
@@ -1,19 +1,35 @@
 package metadoc.cli
 
 import java.io.File
+import java.io.OutputStreamWriter
+import java.io.PrintStream
+import java.nio.file.FileVisitResult
 import java.nio.file.Files
-import java.nio.file.StandardCopyOption
+import java.nio.file.Path
+import java.nio.file.SimpleFileVisitor
 import java.nio.file.StandardOpenOption
 import java.util.zip.{ZipEntry, ZipInputStream}
 import scala.collection.mutable
 import org.langmeta._
 import org.langmeta.internal.io.PathIO
 import caseapp.{Name => _, _}
+import java.nio.file.attribute.BasicFileAttributes
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentSkipListSet
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicReference
+import java.util.function.UnaryOperator
+import java.util.function.{Function => JFunction}
+import java.{util => ju}
+import scala.collection.GenSeq
+import scala.collection.mutable.ArrayBuffer
+import scala.util.control.NonFatal
+import caseapp.{Name => _}
+import caseapp._
+import metadoc.schema
 import metadoc.{schema => d}
-import better.files._
-import org.langmeta.internal.inputs._
-import metadoc.schema.Ranges
-import org.langmeta.internal.io.FileIO
+import org.langmeta._
+import org.langmeta.internal.semanticdb.{schema => s}
 
 @AppName("metadoc")
 @AppVersion("0.1.0-SNAPSHOT")
@@ -25,55 +41,171 @@ case class MetadocOptions(
       "Clean the target directory before generating new site. " +
         "All files will be deleted so be careful."
     )
-    cleanTargetFirst: Boolean = false
+    cleanTargetFirst: Boolean = false,
+    @HelpMessage("Disable fancy progress bar")
+    nonInteractive: Boolean = false
 )
 
-case class MetadocSite(
-    semanticdb: Seq[AbsolutePath],
-    symbols: Seq[d.Symbol],
-    index: d.Index
-)
-
-object MetadocCli extends CaseApp[MetadocOptions] {
-  def filename(input: Input): String = input match {
-    case Input.VirtualFile(path, _) => path
-    case Input.File(path, _) =>
-      path.toRelative(PathIO.workingDirectory).toString()
-    case els => els.syntax
+class CliRunner(paths: GenSeq[AbsolutePath], options: MetadocOptions) {
+  val target = AbsolutePath(
+    options.target.getOrElse(sys.error("--target is required"))
+  )
+  private val display = new TermDisplay(
+    new OutputStreamWriter(System.out),
+    fallbackMode = options.nonInteractive || TermDisplay.defaultFallbackMode
+  )
+  private val semanticdb = target.resolve("semanticdb")
+  private val symbolRoot = target.resolve("symbol")
+  private type Symbol = String
+  private val filenames = new ConcurrentSkipListSet[String]()
+  private val symbols =
+    new ConcurrentHashMap[Symbol, AtomicReference[d.Symbol]]()
+  private val mappingFunction =
+    new JFunction[Symbol, AtomicReference[d.Symbol]] {
+      override def apply(t: Symbol): AtomicReference[schema.Symbol] =
+        new AtomicReference(d.Symbol(symbol = t))
+    }
+  private def addDefinition(symbol: Symbol, position: d.Position): Unit = {
+    val value = symbols.computeIfAbsent(symbol, mappingFunction)
+    value.getAndUpdate(new UnaryOperator[d.Symbol] {
+      override def apply(t: schema.Symbol): schema.Symbol =
+        t.definition.fold(t.copy(definition = Some(position))) { _ =>
+          // Do nothing, conflicting symbol definitions, for example js/jvm
+          t
+        }
+    })
+  }
+  private def addReference(
+      filename: String,
+      range: d.Range,
+      symbol: Symbol
+  ): Unit = {
+    val value = symbols.computeIfAbsent(symbol, mappingFunction)
+    value.getAndUpdate(new UnaryOperator[d.Symbol] {
+      override def apply(t: d.Symbol): d.Symbol = {
+        val ranges = t.references.getOrElse(filename, d.Ranges())
+        val newRanges = ranges.copy(ranges.ranges :+ range)
+        val newReferences = t.references + (filename -> newRanges)
+        t.copy(references = newReferences)
+      }
+    })
   }
 
-  def metadocPosition(position: Position): d.Position =
-    d.Position(
-      filename(position.input),
-      position.start,
-      position.end
-    )
-
-  def getSymbols(db: Database): Seq[d.Symbol] = {
-    val symbols = mutable.Map
-      .empty[String, d.Symbol]
-      .withDefault(sym => d.Symbol(symbol = sym))
-
-    def add(name: ResolvedName): Unit = {
-      val syntax = name.symbol.syntax
-      if (name.isDefinition) {
-        symbols(syntax) = symbols(syntax).copy(
-          definition = Some(metadocPosition(name.position))
-        )
-      } else {
-        val old = symbols(syntax)
-        val label = filename(name.position.input)
-        val ranges = old.references.getOrElse(label, Ranges())
-        val newRefences = old.references.updated(
-          label,
-          ranges.addRanges(d.Range(name.position.start, name.position.end))
-        )
-        symbols(syntax) = old.copy(references = newRefences)
+  def buildSymbolIndex(): Unit = {
+    val task = "Building symbol index"
+    display.startTask(task, new File("target"))
+    display.taskLength(task, paths.length, 0)
+    val counter = new AtomicInteger()
+    paths.foreach { path =>
+      try {
+        display.taskProgress(task, counter.getAndIncrement())
+        val bytes = path.readAllBytes
+        val db = s.Database.parseFrom(bytes)
+        db.documents.foreach { document =>
+          document.names.foreach {
+            case s.ResolvedName(_, sym, _)
+                if !sym.endsWith(".") && !sym.endsWith("#") =>
+            // Do nothing, local symbol.
+            case s.ResolvedName(Some(s.Position(start, end)), sym, true) =>
+              addDefinition(sym, d.Position(document.filename, start, end))
+            case s.ResolvedName(Some(s.Position(start, end)), sym, false) =>
+              addReference(document.filename, d.Range(start, end), sym)
+            case _ =>
+          }
+          val out = semanticdb.resolve(document.filename)
+          Files.createDirectories(out.toNIO.getParent)
+          Files.write(out.toNIO, s.Database(document :: Nil).toByteArray)
+          filenames.add(document.filename)
+        }
+      } catch {
+        case NonFatal(e) =>
+          System.err.println(s"$path")
+          val shortTrace = e.getStackTrace.take(10)
+          e.setStackTrace(shortTrace)
+          e.printStackTrace(new PrintStream(System.err))
       }
     }
-    db.names.foreach(add)
-    symbols.values.filter(_.definition.isDefined).toSeq
   }
+
+  def writeSymbolIndex(): Unit = {
+    import scala.collection.JavaConverters._
+    val task = "Writing symbol index"
+    display.startTask(task, new File("target"))
+    display.taskLength(task, symbols.size(), 0)
+    var counter = 0
+    symbols.asScala.foreach {
+      case (_, ref) =>
+        val symbol = ref.get()
+        display.taskProgress(task, { counter += 1; counter })
+        if (symbol.definition.isDefined) {
+          val url = MetadocCli.encodeSymbolName(symbol.symbol)
+          val out = symbolRoot.resolve(url)
+          Files.createDirectories(out.toNIO.getParent)
+          Files.write(
+            out.toNIO,
+            ref.get.toByteArray,
+            StandardOpenOption.CREATE
+          )
+        }
+    }
+  }
+
+  def writeFilesnames(): Unit = {
+    import scala.collection.JavaConverters._
+    val index = d.Index(files = filenames.asScala.toSeq)
+    Files.write(
+      target.resolve("metadoc.index").toNIO,
+      index.toByteArray,
+      StandardOpenOption.TRUNCATE_EXISTING,
+      StandardOpenOption.CREATE
+    )
+  }
+
+  def writeAssets(): Unit = {
+    val root = target.toNIO
+    val inputStream = MetadocCli.getClass.getClassLoader
+      .getResourceAsStream("metadoc-assets.zip")
+    if (inputStream == null)
+      sys.error("Failed to locate metadoc-assets.zip on the classpath")
+    val zipStream = new ZipInputStream(inputStream)
+    val bytes = new Array[Byte](8012)
+
+    Stream
+      .continually(zipStream.getNextEntry)
+      .takeWhile(_ != null)
+      .filterNot(_.isDirectory)
+      .foreach { entry =>
+        val path = root.resolve(entry.getName)
+        if (Files.notExists(path))
+          Files.createDirectories(path.getParent)
+        val out = Files.newOutputStream(path, StandardOpenOption.CREATE)
+
+        def copyLoop: Unit = {
+          val read = zipStream.read(bytes, 0, bytes.length)
+          if (read > 0) {
+            out.write(bytes, 0, read)
+            copyLoop
+          }
+        }
+
+        copyLoop
+        out.flush()
+        out.close()
+      }
+  }
+
+  def run(): Unit = {
+    display.init()
+    Files.createDirectories(target.toNIO)
+    buildSymbolIndex()
+    writeSymbolIndex()
+    writeFilesnames()
+    writeAssets()
+    display.stop()
+  }
+}
+
+object MetadocCli extends CaseApp[MetadocOptions] {
 
   def encodeSymbolName(name: String): String = {
     val md = java.security.MessageDigest.getInstance("SHA-512")
@@ -82,101 +214,32 @@ object MetadocCli extends CaseApp[MetadocOptions] {
     String.format("%0128x", new java.math.BigInteger(1, sha))
   }
 
-  def createMetadocSite(site: MetadocSite, options: MetadocOptions): Unit = {
-    val target = AbsolutePath(
-      options.target.getOrElse(sys.error("--target is required!"))
-    )
-    if (options.cleanTargetFirst && Files.exists(target.toNIO)) {
-      target.toFile.toScala.delete()
-    }
-    def semanticdb(): Unit = {
-      site.semanticdb.foreach { root =>
-        val from = root.resolve("META-INF").resolve("semanticdb")
-        val to = target.resolve("semanticdb")
-        FileIO.listAllFilesRecursively(from).files.foreach { path =>
-          val in = from.toNIO.resolve(path.toNIO)
-          val out = to.resolve(path).toNIO
-          Files.createDirectories(out.getParent)
-          Files.copy(in, out, StandardCopyOption.REPLACE_EXISTING)
+  def semanticdbs(paths: Seq[AbsolutePath]): ArrayBuffer[AbsolutePath] = {
+    val files = ArrayBuffer.newBuilder[AbsolutePath]
+    val visitor = new SimpleFileVisitor[Path] {
+      override def visitFile(
+          file: Path,
+          attrs: BasicFileAttributes
+      ): FileVisitResult = {
+        if (file.getFileName.toString.endsWith(".semanticdb")) {
+          files += AbsolutePath(file)
         }
+        FileVisitResult.CONTINUE
       }
     }
-
-    def symbol(): Unit = {
-      val root = target.resolve("symbol")
-      root.toFile.mkdirs()
-      site.symbols.foreach { symbol =>
-        val url = encodeSymbolName(symbol.symbol)
-        val out = root.resolve(url)
-        Files.createDirectories(out.toNIO.getParent)
-        Files.write(
-          out.toNIO,
-          symbol.toByteArray,
-          StandardOpenOption.CREATE
-        )
-      }
+    paths.foreach { path =>
+      Files.walkFileTree(path.toNIO, visitor)
     }
-    def index(): Unit = {
-      Files.write(
-        target.resolve("metadoc.index").toNIO,
-        site.index.toByteArray
-      )
-    }
-
-    def assets(): Unit = {
-      val root = target.toNIO
-      val inputStream = MetadocCli.getClass.getClassLoader
-        .getResourceAsStream("metadoc-assets.zip")
-      if (inputStream == null)
-        sys.error("Failed to locate metadoc-assets.zip on the classpath")
-      val zipStream = new ZipInputStream(inputStream)
-      val bytes = new Array[Byte](8012)
-
-      Stream
-        .continually(zipStream.getNextEntry)
-        .takeWhile(_ != null)
-        .filterNot(_.isDirectory)
-        .foreach { entry =>
-          val path = root.resolve(entry.getName)
-          if (Files.notExists(path))
-            Files.createDirectories(path.getParent)
-          val out = Files.newOutputStream(path, StandardOpenOption.CREATE)
-
-          def copyLoop: Unit = {
-            val read = zipStream.read(bytes, 0, bytes.length)
-            if (read > 0) {
-              out.write(bytes, 0, read)
-              copyLoop
-            }
-          }
-
-          copyLoop
-          out.flush()
-          out.close()
-        }
-    }
-
-    semanticdb()
-    symbol()
-    index()
-    assets()
+    files.result()
   }
 
   def run(options: MetadocOptions, remainingArgs: RemainingArgs): Unit = {
-    val classpath = Classpath(
-      remainingArgs.remainingArgs.flatMap { cp =>
-        cp.split(File.pathSeparator).map(AbsolutePath(_))
-      }.toList
-    )
-    val db = Database.load(classpath)
-    val symbols = getSymbols(db)
-    val files = db.documents.collect {
-      case doc if doc.input.isInstanceOf[Input.VirtualFile] =>
-        filename(doc.input)
+    val classpath = remainingArgs.remainingArgs.flatMap { cp =>
+      cp.split(File.pathSeparator).map(AbsolutePath(_))
     }
-    val index = d.Index(files, symbols.map(_.withReferences(Map.empty)))
-    val site = MetadocSite(classpath.shallow, symbols, index)
-    createMetadocSite(site, options)
+    val files = semanticdbs(classpath)
+    val runner = new CliRunner(files.par, options)
+    runner.run()
     println(options.target.get)
   }
 }
