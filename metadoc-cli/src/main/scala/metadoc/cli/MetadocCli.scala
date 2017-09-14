6f146a8b8b0aa37b5f752b560c7f859897f9ac47
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-09-15 01:06:16 +0200

    Refactor cli a bit
    - Abstract over TermDisplay.startTask/progressTask
    - Move file scan to CliRunner.
    - Add some comments
    
    From running metadoc on a corpus of 1M LOC with --zip enabled.
    ```
    Scanning semanticdb files
      100.0% [##########] 507 files processed
    Building symbol index
      100.0% [##########] 8413 files processed
    Writing symbol index
      100.0% [##########] 214560 files processed
    metadoc --target out --zip --clean-target-first   90.83s user 4.83s system 399% cpu 23.966 total
    ```

--- metadoc-cli/src/main/scala/metadoc/cli/MetadocCli.scala
+++ metadoc-cli/src/main/scala/metadoc/cli/MetadocCli.scala
@@ -26,6 +26,7 @@ import java.util.function.{Function => JFunction}
 import java.{util => ju}
 import scala.collection.GenSeq
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.parallel.mutable.ParArray
 import scala.util.control.NonFatal
 import caseapp._
 import caseapp.{Name => _}
@@ -55,11 +56,11 @@ case class MetadocOptions(
 
 case class Target(target: AbsolutePath, onClose: () => Unit)
 
-class CliRunner(paths: GenSeq[AbsolutePath], options: MetadocOptions) {
+class CliRunner(classpath: Seq[AbsolutePath], options: MetadocOptions) {
   require(options.target.isDefined, "--target is required")
-  type Target = (AbsolutePath, () => Unit)
-
   val Target(target, onClose) = if (options.zip) {
+    // For large corpora (>1M LOC) writing the symbol/ directory is the
+    // bottle-neck unless --zip is enabled.
     val out = AbsolutePath(options.target.get).resolve("metadoc.zip")
     Files.createDirectories(out.toNIO.getParent)
     val zipfs = FileSystems.newFileSystem(
@@ -85,6 +86,14 @@ class CliRunner(paths: GenSeq[AbsolutePath], options: MetadocOptions) {
     new ConcurrentHashMap[Symbol, AtomicReference[d.Symbol]]()
   private val mappingFunction: JFunction[Symbol, AtomicReference[d.Symbol]] =
     t => new AtomicReference(d.Symbol(symbol = t))
+  private def overwrite(out: Path, bytes: Array[Byte]): Unit = {
+    Files.write(
+      out,
+      bytes,
+      StandardOpenOption.TRUNCATE_EXISTING,
+      StandardOpenOption.CREATE
+    )
+  }
   private def addDefinition(symbol: Symbol, position: d.Position): Unit = {
     val value = symbols.computeIfAbsent(symbol, mappingFunction)
     value.getAndUpdate(new UnaryOperator[d.Symbol] {
@@ -110,80 +119,98 @@ class CliRunner(paths: GenSeq[AbsolutePath], options: MetadocOptions) {
       }
     })
   }
-
-  def buildSymbolIndex(): Unit = {
-    val task = "Building symbol index"
+  type Tick = () => Unit
+  private def phase[T](task: String, length: Int)(f: Tick => T): T = {
     display.startTask(task, new File("target"))
-    display.taskLength(task, paths.length, 0)
+    display.taskLength(task, length, 0)
     val counter = new AtomicInteger()
-    paths.foreach { path =>
-      try {
-        display.taskProgress(task, counter.getAndIncrement())
-        val bytes = path.readAllBytes
-        val db = s.Database.parseFrom(bytes)
-        db.documents.foreach { document =>
-          document.names.foreach {
-            case s.ResolvedName(_, sym, _)
-                if !sym.endsWith(".") && !sym.endsWith("#") =>
-            // Do nothing, local symbol.
-            case s.ResolvedName(Some(s.Position(start, end)), sym, true) =>
-              addDefinition(sym, d.Position(document.filename, start, end))
-            case s.ResolvedName(Some(s.Position(start, end)), sym, false) =>
-              addReference(document.filename, d.Range(start, end), sym)
-            case _ =>
+    val tick: Tick = { () =>
+      display.taskProgress(task, counter.incrementAndGet())
+    }
+    val result = f(tick)
+    display.completedTask(task, success = true)
+    result
+  }
+
+  def scanSemanticdbs(): GenSeq[AbsolutePath] =
+    phase("Scanning semanticdb files", classpath.length) { tick =>
+      val files = ParArray.newBuilder[AbsolutePath]
+      val visitor = new SimpleFileVisitor[Path] {
+        override def visitFile(
+            file: Path,
+            attrs: BasicFileAttributes
+        ): FileVisitResult = {
+          if (file.getFileName.toString.endsWith(".semanticdb")) {
+            files += AbsolutePath(file)
           }
-          val out = semanticdb.resolve(document.filename)
-          Files.createDirectories(out.toNIO.getParent)
-          overwrite(
-            out.toNIO
-              .resolveSibling(out.toNIO.getFileName.toString + ".semanticdb"),
-            s.Database(document :: Nil).toByteArray
-          )
-          filenames.add(document.filename)
+          FileVisitResult.CONTINUE
         }
-      } catch {
-        case NonFatal(e) =>
-          System.err.println(s"$path")
-          val shortTrace = e.getStackTrace.take(10)
-          e.setStackTrace(shortTrace)
-          e.printStackTrace(new PrintStream(System.err))
+      }
+      classpath.foreach { path =>
+        tick()
+        Files.walkFileTree(path.toNIO, visitor)
+      }
+      files.result()
+    }
+
+  def buildSymbolIndex(paths: GenSeq[AbsolutePath]): Unit =
+    phase("Building symbol index", paths.length) { tick =>
+      paths.foreach { path =>
+        try {
+          tick()
+          val bytes = path.readAllBytes
+          val db = s.Database.parseFrom(bytes)
+          db.documents.foreach { document =>
+            document.names.foreach {
+              case s.ResolvedName(_, sym, _)
+                  if !sym.endsWith(".") && !sym.endsWith("#") =>
+              // Do nothing, local symbol.
+              case s.ResolvedName(Some(s.Position(start, end)), sym, true) =>
+                addDefinition(sym, d.Position(document.filename, start, end))
+              case s.ResolvedName(Some(s.Position(start, end)), sym, false) =>
+                addReference(document.filename, d.Range(start, end), sym)
+              case _ =>
+            }
+            val out = semanticdb.resolve(document.filename)
+            Files.createDirectories(out.toNIO.getParent)
+            overwrite(
+              out.toNIO
+                .resolveSibling(
+                  out.toNIO.getFileName.toString + ".semanticdb"
+                ),
+              s.Database(document :: Nil).toByteArray
+            )
+            filenames.add(document.filename)
+          }
+        } catch {
+          case NonFatal(e) =>
+            System.err.println(s"$path")
+            val shortTrace = e.getStackTrace.take(10)
+            e.setStackTrace(shortTrace)
+            e.printStackTrace(new PrintStream(System.err))
+        }
       }
     }
-    display.completedTask(task, success = true)
-  }
 
-  private def overwrite(out: Path, bytes: Array[Byte]): Unit = {
-    Files.write(
-      out,
-      bytes,
-      StandardOpenOption.TRUNCATE_EXISTING,
-      StandardOpenOption.CREATE
-    )
-  }
-
-  def writeSymbolIndex(): Unit = {
-    import scala.collection.JavaConverters._
-    val task = "Writing symbol index"
-    display.startTask(task, new File("target"))
-    display.taskLength(task, symbols.size(), 0)
-    var counter = 0
-    symbols.asScala.foreach {
-      case (_, ref) =>
-        val symbol = ref.get()
-        display.taskProgress(task, { counter += 1; counter })
-        if (symbol.definition.isDefined) {
-          val url = MetadocCli.encodeSymbolName(symbol.symbol)
-          val out = symbolRoot.resolve(url)
-          Files.createDirectories(out.toNIO.getParent)
-          Files.write(
-            out.toNIO,
-            ref.get.toByteArray,
-            StandardOpenOption.CREATE
-          )
-        }
+  def writeSymbolIndex(): Unit =
+    phase("Writing symbol index", symbols.size()) { tick =>
+      import scala.collection.JavaConverters._
+      Files.createDirectory(symbolRoot.toNIO)
+      symbols.asScala.foreach {
+        case (_, ref) =>
+          tick()
+          val symbol = ref.get()
+          if (symbol.definition.isDefined) {
+            val url = MetadocCli.encodeSymbolName(symbol.symbol)
+            val out = symbolRoot.resolve(url)
+            Files.write(
+              out.toNIO,
+              ref.get.toByteArray,
+              StandardOpenOption.CREATE
+            )
+          }
+      }
     }
-    display.completedTask(task, success = true)
-  }
 
   def writeAssets(): Unit = {
     val root = target.toNIO
@@ -193,7 +220,6 @@ class CliRunner(paths: GenSeq[AbsolutePath], options: MetadocOptions) {
       sys.error("Failed to locate metadoc-assets.zip on the classpath")
     val zipStream = new ZipInputStream(inputStream)
     val bytes = new Array[Byte](8012)
-
     Stream
       .continually(zipStream.getNextEntry)
       .takeWhile(_ != null)
@@ -204,15 +230,15 @@ class CliRunner(paths: GenSeq[AbsolutePath], options: MetadocOptions) {
           Files.createDirectories(path.getParent)
         val out = Files.newOutputStream(path, StandardOpenOption.CREATE)
 
-        def copyLoop: Unit = {
+        def copyLoop(): Unit = {
           val read = zipStream.read(bytes, 0, bytes.length)
           if (read > 0) {
             out.write(bytes, 0, read)
-            copyLoop
+            copyLoop()
           }
         }
 
-        copyLoop
+        copyLoop()
         out.flush()
         out.close()
       }
@@ -228,7 +254,8 @@ class CliRunner(paths: GenSeq[AbsolutePath], options: MetadocOptions) {
     try {
       display.init()
       Files.createDirectories(target.toNIO)
-      buildSymbolIndex()
+      val paths = scanSemanticdbs()
+      buildSymbolIndex(paths)
       writeSymbolIndex()
       writeAssets()
       writeWorkspace()
@@ -248,31 +275,11 @@ object MetadocCli extends CaseApp[MetadocOptions] {
     String.format("%0128x", new java.math.BigInteger(1, sha))
   }
 
-  def semanticdbs(paths: Seq[AbsolutePath]): ArrayBuffer[AbsolutePath] = {
-    val files = ArrayBuffer.newBuilder[AbsolutePath]
-    val visitor = new SimpleFileVisitor[Path] {
-      override def visitFile(
-          file: Path,
-          attrs: BasicFileAttributes
-      ): FileVisitResult = {
-        if (file.getFileName.toString.endsWith(".semanticdb")) {
-          files += AbsolutePath(file)
-        }
-        FileVisitResult.CONTINUE
-      }
-    }
-    paths.foreach { path =>
-      Files.walkFileTree(path.toNIO, visitor)
-    }
-    files.result()
-  }
-
   def run(options: MetadocOptions, remainingArgs: RemainingArgs): Unit = {
     val classpath = remainingArgs.remainingArgs.flatMap { cp =>
       cp.split(File.pathSeparator).map(AbsolutePath(_))
     }
-    val files = semanticdbs(classpath)
-    val runner = new CliRunner(files.par, options)
+    val runner = new CliRunner(classpath, options)
     runner.run()
     println(options.target.get)
   }
