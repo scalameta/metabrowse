386c9a0e9cfde1191353fa7b3285dc7bc473dd0d
Jonas Fonseca <jonas.fonseca@gmail.com>
2018-02-17 11:53:02 -0500

    Only deploy as part of the ci-test matrix entry

--- bin/ci-publish.sh
+++ bin/ci-publish.sh
@@ -5,7 +5,7 @@ if [[ "$TRAVIS_SECURE_ENV_VARS" == true && "$CI_PUBLISH" == true ]]; then
   echo "Publishing..."
   git log | head -n 20
   echo "$PGP_SECRET" | base64 --decode | gpg --import
-  if [ -n "$TRAVIS_TAG" ]; then
+  if [[ -n "$TRAVIS_TAG" ]]; then
     echo "Tag push, publishing stable release to Sonatype."
     sbt ci-release sonatypeReleaseAll
   elif [[ "$TRAVIS_BRANCH" == "master" ]]; then
